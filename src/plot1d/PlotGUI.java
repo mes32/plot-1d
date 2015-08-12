801ef66c8941447d33885e55168acca5797f2fde
mes32 <stockman.mike@gmail.com>
2015-08-12 09:25:43 -0400

    Added the PlotGUI class where the main plot-1d GUI will be built

---
+++ src/plot1d/PlotGUI.java
@@ -0,0 +1,16 @@
+/**
+ *  PlotGUI.java - plot-1d
+ *
+ *  This is the main GUI that shows the results of plotting.
+ *
+ */
+
+package plot1d;
+
+import java.io.*;
+
+public class PlotGUI {
+    public PlotGUI() {
+        System.out.println("PlotGUI.java");
+    }
+}
