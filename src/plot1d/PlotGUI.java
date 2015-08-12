e2ab369b89a2f5e6a376c841973acacaa8887e0b
mes32 <stockman.mike@gmail.com>
2015-08-12 09:49:58 -0400

    Reformatted comments in PlotGUI

--- src/plot1d/PlotGUI.java
+++ src/plot1d/PlotGUI.java
@@ -1,7 +1,7 @@
-/**
- *  PlotGUI.java - plot-1d
+/*
+ *  PlotGUI.java
  *
- *  This is the main GUI that shows the results of plotting.
+ *  This class is part of the package plot1d
  *
  */
 
@@ -9,6 +9,9 @@ package plot1d;
 
 import java.io.*;
 
+/**
+ * This is the main plot-1d GUI in which the results of plotting will be displayed
+ */
 public class PlotGUI {
     public PlotGUI() {
         System.out.println("PlotGUI.java");
