e33420cfdb6eb18b0445ddf934ed38501cd5206d
mes32 <stockman.mike@gmail.com>
2015-08-12 09:01:28 -0400

    Added launcher bash script

---
+++ plot-1d.sh
@@ -0,0 +1,15 @@
+
+
+# Absolute path to this script
+# A roughly portable version of "readlink -f"
+SCRIPT="${PWD}/${0}"
+
+# Absolute path to the directory this script is in
+PROJECT_DIR=`dirname ${SCRIPT}`
+
+# Absolute path to compiled class files
+BUILD_DIR=${PROJECT_DIR}/build/classes/
+
+CLASSPATH=${BUILD_DIR}
+
+java -cp "${CLASSPATH}" Plot1dApp
