5219632c7e58c8bb6dd20c1952a065db8b11b4bf
mes32 <stockman.mike@gmail.com>
2015-08-24 09:03:38 -0400

    Add interval labels to vertical axes

--- src/plot1d/VerticalAxis.java
+++ src/plot1d/VerticalAxis.java
@@ -19,7 +19,11 @@ import plot1d.*;
  */
 abstract class VerticalAxis extends AbstractAxis {
 
+    private static final Color LABEL_COLOR = new Color(0, 0, 0);
+    private static final Font LABEL_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
+
     private int axisX;
+    private String label;
 
     private int minX;
     private int maxX;
@@ -28,6 +32,7 @@ abstract class VerticalAxis extends AbstractAxis {
 
     public VerticalAxis(double x, MappingToGUI trans) {
         axisX = trans.mapX(x);
+        label = String.valueOf(x);
 
         minX = trans.getBox().getMinX();
         maxX = trans.getBox().getMaxX();
@@ -40,6 +45,15 @@ abstract class VerticalAxis extends AbstractAxis {
     public void draw(Graphics g) {
         g.setColor(this.getColor());
         g.drawLine(axisX, minY, axisX, maxY + minY);
+
+        g.setColor(LABEL_COLOR);
+        g.setFont(LABEL_FONT);
+        FontMetrics metric = g.getFontMetrics(LABEL_FONT);
+        int width = metric.stringWidth(label);
+        int height = metric.getAscent();
+        int x = axisX - (width / 2);
+        int y = maxY + (height / 2) + 25;
+        g.drawString(label, x, y);
     }
 }
 
