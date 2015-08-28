fdf951cc4f56422c78eb1ceca21136e3da77d3d7
mes32 <stockman.mike@gmail.com>
2015-08-28 10:15:52 -0400

    Added some comments to PlotFrame class

--- src/plot1d/gui/PlotFrame.java
+++ src/plot1d/gui/PlotFrame.java
@@ -1,13 +1,12 @@
 /*
     PlotFrame.java
 
-    This class is part of the package plot1d
+    This class is part of the program plot-1d
 
  */
 
 package plot1d.gui;
 
-import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,7 +15,6 @@ import java.awt.*;
  * This is an extension of JFrame for use by PlotGUI
  */
 public class PlotFrame extends JFrame {
-
     public PlotFrame(String plottedFileName) {
         setTitle(plottedFileName + "  -  plot-1d");
         setResizable(false);
